18c704e80af65ab711e6cffe5282d3f37d63359f
Zerotask <Zerotask@users.noreply.github.com>
2021-05-15 15:03:40 +0200

    chore: removed de stages

--- src/routes/de/index.svelte
+++ src/routes/de/index.svelte
@@ -8,6 +8,7 @@
 
 <section>
 	<h1>Willkommen im Rust-Dschungel!</h1>
+	<p>Diese Seite befindet sich noch im Aufbau. Du kannst alternativ die englische Seite nutzen.</p>
 </section>
 
 <section>
--- src/routes/de/stages/1/summary.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Lesson from '$components/lesson.svelte';
-</script>
-
-<Lesson title="Stage 1: Summary">
-	<p>TODO</p>
-</Lesson>
