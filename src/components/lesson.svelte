147628c1e73aea1947545dfa7d36c17fc98eee0c
Zerotask <Zerotask@users.noreply.github.com>
2021-05-23 11:10:06 +0200

    chore: added title to prev / next lesson links

--- src/components/lesson.svelte
+++ src/components/lesson.svelte
@@ -74,11 +74,19 @@
 
 <div class="lesson-nav">
 	{#if previous}
-		<a sveltekit:prefetch class="previous" href={previous} rel="prev">❮ Previous</a>
+		<a
+			sveltekit:prefetch
+			class="previous"
+			href={previous}
+			rel="prev"
+			title="Go to the previous lesson">❮ Previous</a
+		>
 	{/if}
 
 	{#if next}
-		<a sveltekit:prefetch class="next" href={next} rel="prev">Next ❯</a>
+		<a sveltekit:prefetch class="next" href={next} rel="prev" title="Go to the next lesson"
+			>Next ❯</a
+		>
 	{/if}
 </div>
 
