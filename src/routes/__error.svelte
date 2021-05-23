4bbe4050c85c3d7d526080d2e8911724849f7823
Zerotask <Zerotask@users.noreply.github.com>
2021-05-23 10:51:12 +0200

    chore: minor error page improvement

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -5,9 +5,15 @@
 <div class="columns">
 	<section class="column col-12">
 		<h1>Something went wrong!</h1>
+		<h4>You can go back <a href="/">to the homepage</a>.</h4>
 
 		<p>
-			<img src="/images/ferris-gesture.png" alt="Rust masco ferris gesture" width="400" />
+			<img
+				src="/images/ferris-gesture.png"
+				alt="Rust mascot ferris gesture"
+				title="Rust mascot ferris gesture"
+				width="350"
+			/>
 		</p>
 	</section>
 </div>
