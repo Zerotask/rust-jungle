91a78317c5573118d7d6186d356b86d58a7d581f
Zerotask <Zerotask@users.noreply.github.com>
2021-05-24 10:57:48 +0200

    chore: minor changes

--- src/components/ferris-reading.svelte
+++ src/components/ferris-reading.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let width = 350;
+	let windowWidth: number;
+
+	onMount(() => {
+		// Update width if device width is very small.
+		width = Math.min(width, windowWidth - 40);
+	});
 </script>
 
+<svelte:window bind:outerWidth={windowWidth} />
 <img
 	src="/images/ferris-reading.png"
 	alt="Rust mascot Ferris reading a book"
--- src/components/ferris.svelte
+++ src/components/ferris.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let width = 350;
+	let windowWidth: number;
+
+	onMount(() => {
+		// Update width if device width is very small.
+		width = Math.min(width, windowWidth - 40);
+	});
 </script>
 
+<svelte:window bind:outerWidth={windowWidth} />
 <img src="/images/ferris.png" alt="Rust mascot Ferris" title="Rust mascot Ferris" {width} />
