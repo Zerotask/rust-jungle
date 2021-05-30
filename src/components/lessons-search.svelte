7de1148bbad4f85e3c4c7173f6c57f7717a73579
Zerotask <Zerotask@users.noreply.github.com>
2021-05-30 15:31:39 +0200

    style: lint fix

--- src/components/lessons-search.svelte
+++ src/components/lessons-search.svelte
@@ -11,7 +11,7 @@
 	let searchValue: string = '';
 
 	onMount(() => {
-		// consider URL query 
+		// consider URL query
 		const searchQuery = $page.query.get('search');
 		if (searchQuery) {
 			search(searchQuery);
