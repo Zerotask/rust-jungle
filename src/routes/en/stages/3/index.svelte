3a6473dea4772cea9da8c0c952d42db69549f985
Zerotask <Zerotask@users.noreply.github.com>
2021-06-04 19:58:54 +0200

    chore: renamed stage 3

--- src/routes/en/stages/3/index.svelte
+++ src/routes/en/stages/3/index.svelte
@@ -2,9 +2,9 @@
 	import Lesson from '$components/lesson/lesson.svelte';
 </script>
 
-<Lesson title="Basic Control Flow" next="if-else">
+<Lesson title="Control Structures" next="if-else">
 	<p>
-		In this chapter let's talk about basic control flow methods in Rust. If you are familiar with C
-		based languages you'll feel right at home and maybe enjoy a surprise or two.
+		In this chapter let's talk about the basic control structures in Rust. If you are familiar with
+		C based languages you'll feel right at home and maybe enjoy a surprise or two.
 	</p>
 </Lesson>
